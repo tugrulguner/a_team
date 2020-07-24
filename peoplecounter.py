b3a8ad6135b84f2790051d544efd57efe32b1c4b
Tugrul Guner <47668043+tugrulguner@users.noreply.github.com>
2020-07-24 14:24:30 -0400

    Update peoplecounter.py

--- peoplecounter.py
+++ peoplecounter.py
@@ -24,3 +24,25 @@ def peoplecounter(path):
 	padding=(8, 8), scale=1.05)
 	# draw the original bounding boxes
   return rects.shape[0]
+
+### for visualization purposes ###
+#for (x, y, w, h) in rects:
+#	cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	# apply non-maxima suppression to the bounding boxes using a
+	# fairly large overlap threshold to try to maintain overlapping
+	# boxes that are still people
+#rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+#pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+	# draw the final bounding boxes
+#for (xA, yA, xB, yB) in pick:
+#	cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+	# show some information on the number of bounding boxes
+#filename = imagePath[imagePath.rfind("/") + 1:]
+#print("[INFO] {}: {} original boxes, {} after suppression".format(
+#	filename, len(rects), len(pick)))
+	# show the output images
+#cv2_imshow(orig)
+#cv2_imshow(image)
+#cv2.waitKey(0)
+
+###
