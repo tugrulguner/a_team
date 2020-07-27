c1bf6ebadc40beeb295d6b1da387974f66b010ea
Tugrul Guner <47668043+tugrulguner@users.noreply.github.com>
2020-07-26 23:11:54 -0400

    Update yolo_main.py

--- yolo_main.py
+++ yolo_main.py
@@ -21,21 +21,8 @@ import matplotlib.pyplot as plt
 
 def detect_people(frame, net, ln, personIdx=0):
     
-  # base path to YOLO directory
-  MODEL_PATH = '/content/drive/My Drive/Colab_Notebooks/yolo-coco'
-
-  # initialize minimum probability to filter weak detections along with
-  # the threshold when applying non-maxima suppression
   MIN_CONF = 0.3
   NMS_THRESH = 0.3
-
-  # boolean indicating if NVIDIA CUDA GPU should be used
-  USE_GPU = True
-
-  # define the minimum safe distance (in pixels) that two people can be
-  # from each other
-  MIN_DISTANCE = 50
-
   # grab the dimensions of the frame and  initialize the list of
 	# results
   (H, W) = frame.shape[:2]
@@ -110,60 +97,72 @@ def detect_people(frame, net, ln, personIdx=0):
 
 def distvio(image):
 
+    # base path to YOLO directory
+    MODEL_PATH = '/content/drive/My Drive/Colab_Notebooks/yolo-coco'
+    # load the COCO class labels our YOLO model was trained on 
+    labelsPath = os.path.sep.join([MODEL_PATH, "coco.names"])
+    LABELS = open(labelsPath).read().strip().split("\n")
+    # initialize minimum probability to filter weak detections along with
+    # the threshold when applying non-maxima suppression
 
-	# load the COCO class labels our YOLO model was trained on
-	labelsPath = os.path.sep.join([MODEL_PATH, "coco.names"])
-	LABELS = open(labelsPath).read().strip().split("\n")
+
+    
+    # boolean indicating if NVIDIA CUDA GPU should be used
+    USE_GPU = True
+
+    # define the minimum safe distance (in pixels) that two people can be
+    # from each other
+    MIN_DISTANCE = 50
 
 	# derive the paths to the YOLO weights and model configuration
-	weightsPath = os.path.sep.join([MODEL_PATH, "yolov3.weights"])
-	configPath = os.path.sep.join([MODEL_PATH, "yolov3.cfg"])
+    weightsPath = os.path.sep.join([MODEL_PATH, "yolov3.weights"])
+    configPath = os.path.sep.join([MODEL_PATH, "yolov3.cfg"])
 
 	# load our YOLO object detector trained on COCO dataset (80 classes)
-	print("[INFO] loading YOLO from disk...")
-	net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
 	# check if we are going to use GPU
-	if USE_GPU:
+    if USE_GPU:
 	# set CUDA as the preferable backend and target
-		print("[INFO] setting preferable backend and target to CUDA...")
-		net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-		net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        print("[INFO] setting preferable backend and target to CUDA...")
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 	# determine only the *output* layer names that we need from YOLO
-	ln = net.getLayerNames()
-	ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 	# loop over the frames from the video stream
-	frame = image
+    frame = image
 	# resize the frame and then detect people (and only people) in it
-	frame = imutils.resize(frame, width=700)
-	results = detect_people(frame, net, ln,
-	personIdx=LABELS.index("person"))
+    frame = imutils.resize(frame, width=700)
+    results = detect_people(frame, net, ln,
+                            personIdx=LABELS.index("person"))
  
-	bboxes = []
-	for i in results:
-		bboxes.append(i[1])	
+    bboxes = []
+    for i in results:
+        bboxes.append(i[1])	
 	# ensure there are *at least* two people detections (required in
 	# order to compute our pairwise distance maps)
-	if len(results) >= 2:
+    if len(results) >= 2:
 	# extract all centroids from the results and compute the
 	# Euclidean distances between all pairs of the centroids
-		centroids = np.array([r[2] for r in results])
-		D = dist.cdist(centroids, centroids, metric="euclidean")
+        centroids = np.array([r[2] for r in results])
+        D = dist.cdist(centroids, centroids, metric="euclidean")
 
 		# loop over the upper triangular of the distance matrix
-		violation = 0
-		for i in range(0, D.shape[0]):
-			for j in range(i + 1, D.shape[1]):
+        violation = 0
+        for i in range(0, D.shape[0]):
+            for j in range(i + 1, D.shape[1]):
 				# check to see if the distance between any two
 				# centroid pairs is less than the configured number
 				# of pixels
-				if D[i, j] < MIN_DISTANCE:
+                if D[i, j] < MIN_DISTANCE:
 					# update our violation set with the indexes of
 					# the centroid pairs
-					violation += 1
-	return violation, np.array(bboxes)
+                    violation += 1
+    return violation, np.array(bboxes)
 
 def MaskDetector(bboxes, image, tolerance):
     def prepImg(img):
@@ -190,14 +189,15 @@ def MaskDetector(bboxes, image, tolerance):
 ### Main BODY ###
 
 def main():
+	# base path to YOLO directory
+  #MODEL_PATH = '/content/drive/My Drive/Colab_Notebooks/yolo-coco'
 	# Here we can define a single image for test purposes, if you want to check it, you can uncomment below
   image = cv2.imread('/content/drive/My Drive/Colab_Notebooks/test.jpg')
-  #urlden = 'https://a-team-mall-api.herokuapp.com/density'
-  #urlmask = 'https://a-team-mall-api.herokuapp.com/mask'
-
+  urlden = 'https://a-team-mall-api.herokuapp.com/density'
+  urlmask = 'https://a-team-mall-api.herokuapp.com/mask'
   while True:
     # Here I provided a link from https://www.insecam.org/, which is a good spot from Colorado, USA
-    #cap = cv2.VideoCapture('http://208.139.200.133/mjpg/video.mjpg#.XxzWNVzDvVY.link')
+    #cap = cv2.VideoCapture('https://www.insecam.org/en/view/811066/#.Xx5CgDv0_Lk.link')
     #try:
     #  _, image = cap.read()
     #except Exception as e:
@@ -228,8 +228,8 @@ def main():
                        'nomask': maskamount[1]
                        }
     # Here I am just trying to check whether I can post the data to our app
-    #gg = requests.post(urlden, json = facecountstreaming)
-    #zz = requests.post(urlmask, json = maskstreaming)
+    gg = requests.post(urlden, json = facecountstreaming)
+    zz = requests.post(urlmask, json = maskstreaming)
     # print(gg.status_code)
     #if zz.status_code == requests.codes.ok:
     #  print('Mask Uploaded')
